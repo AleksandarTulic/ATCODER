ee253c5796fadbf1c12e255b88068715d7d2a558
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-08-14 17:54:52 +0200

    Create B.cpp

---
+++ Beginner_Contest_137/B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <queue>
+#define ull unsigned long long
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+ 
+    for (int i=k-n+1;i<k+n;i++) cout<<i<<" ";
+    return 0;
+}
