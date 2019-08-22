7fee320240eee9cb84c56498ea29d2adf4b14c66
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-08-22 20:30:31 +0200

    Create A.cpp

---
+++ Beginer_Contest_43/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<(n*(n+1))/2<<endl;
+    return 0;
+}
